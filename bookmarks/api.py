8a01a57b0cf46ae53b3b77c294ed0500a7066525
CecileSun <cecile829@gmail.com>
2015-06-01 18:14:15 +0900

    improves api readability and output style

--- bookmarks/api.py
+++ bookmarks/api.py
@@ -1,24 +1,39 @@
+from django.core.serializers.json import DjangoJSONEncoder
+import json
+from tastypie.serializers import Serializer
 from tastypie.resources import ModelResource
 from tastypie import fields
 from tastypie.authorization import DjangoAuthorization
-from tastypie.constants import ALL
 from models import bookmark, tag
+
+
+# Make tastypie API output Json more readable
+class PrettyJSONSerializer(Serializer):
+    json_indent = 4
+
+    def to_json(self, data, options=None):
+        options = options or {}
+        data = self.to_simple(data, options)
+        return json.dumps(data, cls=DjangoJSONEncoder,
+                         sort_keys=True, ensure_ascii=False, indent=self.json_indent)
 
 
 class tagResource(ModelResource):
     class Meta:
         queryset = tag.objects.all()
+        serializer = PrettyJSONSerializer()
+        # fields = ['tag_name']
         resource_name = 'tag'
         authorization = DjangoAuthorization()
-        #fields=['tag_name']
+
 
 class bookmarkResource(ModelResource):
-    tag = fields.ForeignKey(tagResource, 'tag', full=True)
-
+    tag = fields.ToOneField(tagResource, 'tag', full=True)
+    #tag = fields.CharField(attribute='tag_name')
     class Meta:
         queryset = bookmark.objects.all()
+        serializer = PrettyJSONSerializer()
         resource_name = 'bookmark'
         authorization = DjangoAuthorization()
-        allowed_methods = ['get', 'post']
 
         #fields = ['description','where', 'tag__tag_name']
--- bookmarks/static/script_bookMark.js
+++ bookmarks/static/script_bookMark.js
@@ -1,17 +1,73 @@
-jQuery(function($){
-	//Code using $ aliasing to jQuery
-	$('#button_add').on('click', function(){
-	    $('#bm_info').css('visibility', 'visible');
+$(document).ready(function(){
+    var $myTags = $('#myTags');
+    var tag_list=[];
+    $.ajax({
+        type: 'GET',
+        url: '/bookmarks/api/info/bookmark/?format=json',
+        success: function(data){
+            for (var i=0; i<data['objects'].length; i++){
+                var where = data['objects'][i]['where'];
+                //delete url trailing slash
+                    var re = new RegExp('/$');
+                    if (where.match(re)) {
+                        where = where.substring(0,where.length-1);
+                    };
+                var description = data['objects'][i]['description'];
+                var tag_name = data['objects'][i]['tag']['tag_name'];
+                if (tag_list.indexOf(tag_name)>-1){
+                    $('#'+tag_name).append('<li><a href='+where+'>'+description+'</a><button>delete</button></br></li>');
+                } else {
+                    tag_list.push(data['objects'][i]['tag']['tag_name']);
+                    $myTags.append("<ul class='tag' id="+tag_name+">"+tag_name+"</br></ul>");
+                    $('#'+tag_name).append('<li><a href='+where+'>'+description+'</a><button>delete</button></br></li>');
+                }
+            }
+        }
+    });
+
+//--------------------------------------------DELETE bookmarks DOES NOT WORK----------------------------------------------
+    $('ul').on('click', 'button', function(ev){
+        ev.preventDefault();
+        $(this).parent().remove();
+        var id = $(this).parent().attr('id');
+        console.log(id);
+        $.ajax({
+            type: 'DELETE',
+            url: '/bookmarks/api/info/bookmark/'+id+'/?format=json',
+            success: function(){
+                console.log('deleted!');
+            },
+            error: function(jqXHR, textStatus, errorThrown){
+                console.log(jqXHR);
+            }
+        })
+    });
+//--------------------------------------------DELETE bookmarks DOES NOT WORK----------------------------------------------
+
+
+
+//POST new bookmarks to the server
+    $('#button_add').on('click', function(){
+        $(this).hide();
+	    $('#bm_infoForm').css('visibility', 'visible');
 	});
 
     $('#bm_infoForm').submit(function(ev) {
         ev.preventDefault();
         $(this).hide();
+        // Form validation
         var tag_name_obj = {"tag_name":document.bm_infoForm.tag_name.value};
+        var description = document.bm_infoForm.description.value;
+        var where = document.bm_infoForm.where.value;
+        if (tag_name_obj ==="" || description==="" || where===""){
+            alert("Please enter valid data");
+            $(this).show();
+            return false;
+        }
         var inputData = JSON.stringify({
             "tag": tag_name_obj,
-            "description": document.bm_infoForm.description.value,
-            "where": document.bm_infoForm.where.value,
+            "description": description,
+            "where": where,
         });
         //console.log(inputData);
 
@@ -23,36 +79,15 @@ jQuery(function($){
             processData: false,
             contentType: 'application/json',
             success: function(data) {
-                console.log('post success');
+//                console.log('post success');
             },
             error: function(jqXHR, textStatus, errorThrown){
                 console.log(jqXHR);
             }
         })
+        $('#button_add').show();
+
     });
-
-
-    $.ajax({
-        type: 'GET',
-        url: '/bookmarks/api/info/bookmark/?format=json',
-        success: function(data) {
-            for (var i=0; i<data["objects"].length; i++) {
-                var url=data["objects"][i]["where"];
-                var description = data["objects"][i]["description"];
-                // delete url trailing slash
--               var re = new RegExp('/$')
--               if (url.match(re)) {
--                   url = url.substring(0,url.length-1)
--               }
-                $('#myBookmarks').append("<a href="+url+">"+description+"</a></br>");
-            }
-        },
-
-        error: function(){alert("Error loading bookmarks!")},
-    })
 
 });
 
-//() (jQuery);
-
-//Code using $ aliasing to other lib 
